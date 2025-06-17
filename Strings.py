0418738ee1a13aa2b1d3f4cc2e7bad2404e75eae
Ved-Kanani <vedkanani03@gmail.com>
2025-06-17 10:47:42 +0530

    perform the string functions.

---
+++ Strings.py
@@ -0,0 +1,17 @@
+# num_list="0123456789"
+# print(num_list[0: 7:2])
+
+# chai = "masala chai"
+# print(chai.find("chai"))
+
+# chai = "ginger tea tea tea"
+# print(chai.count("tea"))
+
+# chai = "masala chai"
+# quantity = 2
+# order = "i order {} cups of {}."
+# print(order.format(quantity, chai))
+
+chai = "masala chai"
+slice = chai[0:6:2]  
+print(slice)

9c856f9242feee2f070a7e8fe78bcd577624bd55
Ved-Kanani <vedkanani03@gmail.com>
2025-06-27 17:36:59 +0530

    perform the list .

---
+++ Lists.py
@@ -0,0 +1,22 @@
+# chai = ["masala", "black", "white", "green", "lemon"]
+# print(chai[0:5])
+
+# chai[2] = "ginger"
+# print(chai)
+
+# chai[1:3] = ["mint", "herbal"]
+# print(chai) 
+
+# chai[1:2] = []
+# print(chai)
+
+# for tea in chai:
+#     print(tea)
+
+# if "lemon" in chai:
+#     print("Lemon tea is available")
+# else:
+#     print("tea is not available")
+
+sqaurd_num = [x**2 for x in range(10)]
+print(sqaurd_num)

795c1aa696469ad22a5d37ba10b84542344cd1e1
Ved-Kanani <vedkanani03@gmail.com>
2025-06-27 18:35:57 +0530

    perform the dictionaries.

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+# chai_varities = {"masala": "spicy", "ginger": "strong", "mint": "refreshing"}
+# print(chai_varities)
+
+# print(chai_varities["masala"])
+
+# for chai in chai_varities:
+#     print(chai, chai_varities[chai])
+
+# for key, value in chai_varities.items():
+#     print( key, value)
+
+# if "masala" in chai_varities:
+#     print("Masala tea is available")
+
+# chai_varities["lemon"] = "citrus"
+# print(chai_varities)
+
+# chai_varities.pop("ginger")
+# print(chai_varities)
+
+# del chai_varities["mint"]
+# print(chai_varities)
+
+# tea_shop = {"chai" : {"masala": "spicy", "ginger": "strong", "mint": "refreshing"},
+#             "coffee" : {"black": "bitter", "white": "creamy"}
+# }
+# print(tea_shop)
+# print(tea_shop["chai"])
+
+sqaured_num = {x:x**3 for x in range(5)}
+print(sqaured_num)

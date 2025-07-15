dfa15bba4a8fb7e0076ea8a17b8458731b658eec
Ved-Kanani <vedkanani03@gmail.com>
2025-07-15 16:01:30 +0530

    perform 10 loops problems.

---
+++ Loops.py
@@ -0,0 +1,96 @@
+# numbers = [1,-2,3,7,-9]
+# positive_numbers = 0
+
+# for num in numbers:
+#     if num > 0:
+#         positive_numbers += 1
+
+# print("Total positive numbers:", positive_numbers)
+
+
+
+# n = int(input("Enter a number: "))
+# sum_even = 0
+
+# for i in range(1, n + 1):
+#     if i % 2 == 0:
+#         sum_even =sum_even + 1
+
+# print("Sum of even numbers is : ", sum_even) 
+
+
+
+
+# number = 3
+
+# for i in range(1, 11):
+#     if i == 5:
+#         continue
+#     print(number, "x" , i, "=", number * i)
+
+
+
+# string = "ved"
+
+# reverse_string = ""
+# for char in string:
+#     reverse_string = char + reverse_string
+# print("Reversed string:", reverse_string)
+
+
+
+# input_string = "teetera"
+
+# for char in input_string:
+
+#     if input_string.count(char) == 1:
+#         print("char is:",char)
+#         break
+
+
+
+
+# number = int(input("enter the number :"))
+
+# factorial = 1
+
+# while number > 0 :
+#     factorial = factorial * number
+#     number = number - 1 
+#     print("factorial of number is :" , factorial)
+
+
+# while True:
+#     number = int(input("Enter a number between 1 and 10: "))
+#     if 1 <= number <= 10:
+#         print("Number is in range")
+#         break
+#     else:
+#         print("Number is out of range, try again.")
+
+
+
+
+# number = 3
+
+# if number > 1:
+#     for i in range (1, number):
+#         if (number % i) == 0:
+#             print(number, "is not a prime number")
+#             break
+
+
+
+# items = ["apple", "banana", "cherry"]
+
+# unique_items = set()
+
+# for item in items:
+#     if item in unique_items:
+#         print("Duplicate item:", item)
+#         break
+#     unique_items.add(item)
+
+
+
+

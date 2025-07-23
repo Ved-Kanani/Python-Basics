56e1fb8a906d9fa39fed43f3ea05d79d56d078ff
Ved-Kanani <vedkanani03@gmail.com>
2025-07-23 17:19:44 +0530

    perform Scopes and Closure problems.

---
+++ Scopes&Closure.py
@@ -0,0 +1,21 @@
+#1.Scope
+
+# x=99
+# def func(y):
+#     z=x+y
+#     return z
+
+# result = func
+# print(result(1))
+
+
+
+# # 2.Closure
+
+# def f1():
+#     x=88
+#     def f2():
+#         print(x)
+#     return f2
+# myresult = f1()
+# myresult()
